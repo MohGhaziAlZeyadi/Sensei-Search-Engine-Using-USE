f544a6b03914b938d361a31d23e56886fc6166a1
Moh Al-Zeyadi <johnsmith@gmail.com>
2021-01-25 11:05:29 +0000

    Search eng uesing USE and test with text and voice

--- sre/emp_temp.py
+++
@@ -1,7 +0,0 @@
-import tensorflow_hub as hub
-import tensorflow as tf
-
-print(tf.__version__ )
-
-embed = hub.load("https://tfhub.dev/google/tf2-preview/nnlm-en-dim128/1")
-embeddings = embed(["cat is on the mat", "dog is in the fog"])
